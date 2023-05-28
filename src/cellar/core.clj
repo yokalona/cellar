553e4933e37b0c89132094ddd6dc3910010b533e
Roman Makhlin <roman.v.makhlin@gmail.com>
2023-05-28 22:09:05 +0200

    cellar init

--- src/cellar/core.clj
+++ src/cellar/core.clj
@@ -1,7 +1,8 @@
 (ns cellar.core
   (:require [clojure.java.jdbc :as jdbc]
             [clojure.string :as str]
-            [clojure.test :as test]))
+            [clojure.test :as test]
+            [clojure.pprint :as p]))
 
 (declare ^:dynamic *config*)
 
@@ -23,13 +24,17 @@
        (assoc *config* :tables)
        (set-config!)))
 
+(defn- -transform
+  [columns f]
+  (mapv (fn [[k v]] [(f (if (keyword? k) (name k) k)) v]) columns))
+
 (defn- -table
   ([spec]
    (cond
      (keyword? spec) (if (= :conditional! spec)
                        {:conditional? true}
                        {spec true})
-     (test/function? spec) {:entities spec}
+     (test/function? spec) {:transform spec}
      (coll? spec) {:columns spec}
      :unknown spec))
   ([specs config]
@@ -38,44 +43,46 @@
      (recur (rest specs) (merge config (-table (first specs)))))))
 
 (defn- -init-table
-  [table]
+  [table-name table]
   (jdbc/db-do-commands
     (:db-settings *config*)
-    (jdbc/create-table-ddl (first table)
-                           (:columns (second table))
-                           {:conditional? (:conditional? (second table))
-                            :entities     (:entities (second table))})))
+    (jdbc/create-table-ddl table-name
+                           (-transform (:columns table) (:transform table))
+                           (if (:conditional? table)
+                             {:conditional? true}
+                             {}))))
 
 (defmacro deftable
   [name & specs]
-  (let [new-table (-table specs {})
+  (let [new-table (-table specs {:transform identity})
         columns (->> new-table :columns (mapv (comp symbol first)))]
     (-add-table->config! {(keyword name) new-table})
     (list 'defrecord (symbol (str/capitalize name)) columns)))
 
 (comment
   (deftable flow
-         :conditional!
-         (fn [x] (str/replace (name x) "-" "_"))
-         [[:chat "BIGINT"]
-          [:domain-id "VARCHAR(32)"]
-          [:message "BIGINT"]
-          [:step "VARCHAR(64)"]]))
+            :conditional!
+            (fn [x] (str/replace (name x) "-" "_"))
+            [[:chat "BIGINT"]
+             [:domain-id "VARCHAR(32)"]
+             [:message "BIGINT"]
+             [:step "VARCHAR(64)"]]))
 
 (defn init
-  ([]
-   (let [tables (:tables *config*)]
-     (doseq [table tables]
-       (init (first table)))))
-  ([table]
-   (-init-table (get-in *config* [:tables table]))))
+  ([] (let [tables (:tables *config*)]
+        (doseq [table tables]
+          (init (first table)))))
+  ([table] (-init-table table (get-in *config* [:tables table]))))
 
-(defn exists?
-  [table kvs]
-  (let [where-clause (str/join " AND " (map #(format "%s = ?" %) (map key kvs)))
-        query (format "SELECT * FROM %s WHERE %s LIMIT 1" (name table) where-clause)]
-    (jdbc/query (:db-settings *config*) (cons query (mapv val kvs)))))
+(defn get
+  ([table kvs]
+   (get table kvs nil))
+  ([table kvs limit]
+   (let [where-clause (str/join " AND " (map #(format "%s = ?" (name %)) (map key kvs)))
+         where-clause (if limit (str where-clause " LIMIT " limit) where-clause)
+         query (format "SELECT * FROM %s WHERE %s" (name table) where-clause)]
+     (jdbc/query (:db-settings *config*) (cons query (mapv val kvs))))))
 
-(defn new!
-  [table kvs]
-  (jdbc/insert! (:db-settings *config*) table kvs))
+(defn exists? [table kvs] (some? (seq (get table kvs 1))))
+
+(defn new! [table kvs] (jdbc/insert! (:db-settings *config*) table kvs))
