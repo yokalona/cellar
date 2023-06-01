02ae8e8606de1065ac11bfd0205ec74c534df01d
yokalona <79987661+yokalona@users.noreply.github.com>
2023-06-01 03:21:14 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/java.jdbc "0.7.12"]
-                 [io.github.yokalona/aloop "0.1.0-SNAPSHOT"]]
+                 [io.github.yokalona/aloop "1.0.0"]]
   :repl-options {:init-ns cellar.core})
