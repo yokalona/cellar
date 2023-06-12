dae051d5bface026e42128cd9868c39348dc0cf7
Roman Makhlin <roman.v.makhlin@gmail.com>
2023-06-12 23:48:36 +0200

    cellar

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/java.jdbc "0.7.12"]
-                 [io.github.yokalona/aloop "1.1.5"]]
+                 [io.github.yokalona/aloop "1.1.6"]]
   :repl-options {:init-ns cellar.core})
